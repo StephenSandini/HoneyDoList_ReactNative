a8eb5910cb0e715fad37bfedce291e768c61b5f6
Stephen Sandini <stsandini@gmail.com>
2025-02-01 14:32:41 -0600

    built header component

--- App.js
+++ App.js
@@ -1,41 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
+import Header from './components/header.js';
 
 export default function App() {
-  const [people, setPeople] = useState([
-    {name: 'mario', id:'1'},
-    {name: 'luigi', id:'2'},
-    {name: 'yoshi', id:'3'},
-    {name: 'toad', id:'4'},
-    {name: 'bowser', id:'5'},
-    {name: 'kamek', id:'6'},
-    {name: 'peach', id:'7'},
-    {name: 'daisy', id:'8'},
+  const [todos, setTodos] = useState([
+    {text:'buy coffee', key:'1'},
+    {text:'create an app', key:'2'},
+    {text:'play on the xbox', key:'3'},
   ]);
-
-  const pressHandler = (id) => {
-    setPeople((prevPeople) => {
-      return prevPeople.filter(person => person.id != id);
-    })
-  }
 
   return (
     <View style={styles.container}>
-      
-    <FlatList
-      numColumns={2}
-      keyExtractor={(item) => item.id} 
-      data={people}
-      renderItem={({item}) => (
-        <TouchableOpacity onPress={() => pressHandler(item.id)}>
-          <Text style={styles.item}>{item.name}</Text>
-        </TouchableOpacity>
-      )}  
-    />
-     
-      <StatusBar style="auto" />
-    
+      <Header />
+      <View style={styles.content}>
+        {/* to do form*/}
+        <View style={styles.list}>
+          <FlatList 
+            data={todos}
+            renderItem={({item}) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -44,18 +31,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal: 20,
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'lavender',
-    fontSize: 24,
-    marginHorizontal:10,
-    marginTop: 24,
-  }
- 
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  },
 });
