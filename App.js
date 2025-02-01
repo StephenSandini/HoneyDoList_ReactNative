a274f52ef157003f297f6211b104b326453f517e
Stephen Sandini <stsandini@gmail.com>
2025-02-01 13:31:00 -0600

    basic-touchablecomponents

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
   const [people, setPeople] = useState([
@@ -14,6 +14,12 @@ export default function App() {
     {name: 'daisy', id:'8'},
   ]);
 
+  const pressHandler = (id) => {
+    setPeople((prevPeople) => {
+      return prevPeople.filter(person => person.id != id);
+    })
+  }
+
   return (
     <View style={styles.container}>
       
@@ -22,19 +28,12 @@ export default function App() {
       keyExtractor={(item) => item.id} 
       data={people}
       renderItem={({item}) => (
-        <Text style={styles.item}>{item.name}</Text>
+        <TouchableOpacity onPress={() => pressHandler(item.id)}>
+          <Text style={styles.item}>{item.name}</Text>
+        </TouchableOpacity>
       )}  
     />
-
-      {/* Good for a small list */}
-      {/* <ScrollView>
-        {people.map(item => (
-            <View id={item.id}>
-              <Text style={styles.item}>{item.name}</Text>
-            </View>
-          ))}
-      </ScrollView> */}
-      
+     
       <StatusBar style="auto" />
     
     </View>
